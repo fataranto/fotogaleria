7d301a8605f443f1f364014e4ec90688d04c8cc5
fataranto <fataranto@gmail.com>
2022-05-26 00:34:12 +0200

    funcionalidad fototeca y galería correcta

--- script.js
+++ script.js
@@ -1,67 +1,56 @@
 // Estado de la app
 
 let fotos = ["casa1", "casa2", "casa3", "casa4", "casa5", "casa6", "casa7", "casa8"];
-let galeria = [];
 
+let galeria = []; // creo un array donde guardaré toda la info de las imágenes como objetos, guardaré en todo momento cambios en la misma como ser la ubicación, de manera que al hacer doble click, dependiendo de la ubicación donde se encuentre cambiará y actualizará 
+
+//almaceno como constantes los contenedores #fototeca y #galeria para una más fácil referencia cuando necesite mover una foto de un lugar a otro
 const fototecaCont = document.querySelector("#fototeca");
 const galeriaCont = document.querySelector("#galeria");
 
 
 
 for (let i = 0; i < fotos.length; i++) {
-    // 1. Recuperamos el nombre de la foto
+    // Recuperamos el nombre de la foto desde el array inicial
     let siguienteFoto = fotos[i];
-
+    // creo un objeto "img"...
     let imagen = document.createElement("img");
+    //... y le asigno la ubicación de l aimagen
     imagen.src = `/img/${siguienteFoto}.jpg`;
+    imagen.classList.add("fotoFototeca");
+
+    //cargo la imagen en la fototeca
     fototecaCont.appendChild(imagen);
 
+    //creo un objeto "foto" almacenando los datos que vaya necesitando
     let foto = {
         "nombre" : fotos[i], 
         "src" : `/img/${siguienteFoto}.jpg`,
-        "ubicacion" : fototecaCont
+        "ubicacion" : fototecaCont,
+        "imagen" : imagen,
+        "class" : "fotoFototeca" 
     }
 
+    //agrego el objeto "foto" al array "galeria"
     galeria.push(foto);
-    //galeria[i].nombre = siguienteFoto;
+  
+    //creo un evento "dblclick" a la imagen que según el contenedor donde se encuentra cambia al otro y actualiza su ubicación en el objeto
+    foto.imagen.addEventListener("dblclick", function (event) {
+        if (foto.ubicacion == fototecaCont){
+            foto.ubicacion = galeriaCont;
+            foto.class = "fotoGaleria"
+            imagen.classList.remove("fotoFototeca")
+        }else{
+            foto.ubicacion = fototecaCont;
+            foto.class = "fotoFototeca"
+            imagen.classList.remove("fotoGaleria")
+        }
 
-    //console.log(galeria[i]);
-
-
-    // 1.1 Creo un array de objetos con toda la información relevante de la foto como el nombre, la ubicación (si se encuentra en la galería o no), etc. toda información relevante que vaya necesitando, como por ejemplo asignarle una clase u otra según el lugar donde se encuentre o incluso quitarle o asignarle el listener
-
-    // 2. Creamos un elemento nuevo del tipo 'img' y tenemos que llenar la propiedad .src con la ruta a la imagen
+        foto.imagen.classList.add(foto.class)
+        foto.ubicacion.appendChild(foto.imagen)
+    })
     
 
-    //galeria[i].ubicacion = fototeca;
-
-    //document.querySelector(galeria[i].ubicacion).style.background = "red";
-
-    //console.log(imagen.src);
-/* 
-    imagen.addEventListener("dblclick", function (event) {
-
-        let sectionFototeca = document.querySelector("#fototeca");
-        sectionFototeca.appendChild(imagen);
-
-        let imagen = document.createElement("img");
-        imagen.src = galeria[i];
-
-        // 3. añadir la imagen como hijo de id="galeria"
-        let sectionGaleria = document.querySelector("#galeria");
-        sectionGaleria.appendChild(imagen);
-
-        // 1. Identificar la foto clicada (event.target.src)
-        // 1.5. Añadir el valor del event.target.src en el array de galeria
-        // 2. llamar a la función llenarGaleria()
-
-        //let fotoClicada = event.target.src;
-        //galeria.push(fotoClicada);
-       
-
-    }) */
-
-    // 3. Añadir la imagen como hijo del section id=fototeca
     
 }
 
